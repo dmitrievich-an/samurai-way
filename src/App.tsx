6a1d8d33f1cb06db93ba9edf881b144b65d1eae5
Artem <reebok001@yandex.ru>
2023-11-06 16:32:07 +0300

    again

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,11 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/Header/Header";
-import {Navbar} from "./components/Navbar/Navbar";
-import {Profile} from "./components/Profile/Profile";
-import {Dialogs} from "./components/Dialogs/Dialogs";
-import {Footer} from "./components/Footer/Footer";
-import {Route} from "react-router-dom";
-import {StateType, updateNewPostText} from "./redux/state";
 
-type AppPropsType = {
-  state: StateType
-  addPost: () => void
-  updateNewPostText: (newText: string) => void
-}
-
-function App(props: AppPropsType) {
+function App() {
   return (
-      <div className="app-wrapper">
-        <Header/>
-        <Navbar/>
-        <div className="app-wrapper__content">
-          <Route
-            path="/profile"
-            render={() => <Profile
-              profilePage={props.state.profilePage}
-              addPost={props.addPost}
-              updateNewPostText={props.updateNewPostText}
-            />}
-          />
-          <Route
-            path="/dialogs"
-            render={() => <Dialogs state={props.state.dialogsPage}/>}
-          />
-          {/*<Route path="/news" component={News}/>*/}
-          {/*<Route path="/music" component={Music}/>*/}
-          {/*<Route path="/settings" component={Settings}/>*/}
-        </div>
-        <Footer/>
-      </div>
+    <div className="App">
+     Hello, samurai! Let's go!
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,9 @@
-import './index.css';
-import {state, subscribe} from "./redux/state";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, StateType, updateNewPostText} from "./redux/state";
-import {BrowserRouter} from "react-router-dom";
 
-
-const rerenderEntireTree = (state: StateType) => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
-    </BrowserRouter>,
-    document.getElementById('root')
-  );
-}
-
-export type PostPropsType = {
-  id: number
-  message: string
-  likesCount: number
-}
-export type DialogPropsType = {
-  id: number
-  name: string
-}
-export type MessagePropsType = {
-  id: number
-  message: string
-}
-
-
-
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
